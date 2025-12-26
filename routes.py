ff2b2b3a3d603555d929a9fa7fd32fdfdca2ee90
cyb3erasad <cyb3rasad@gmail.com>
2025-12-26 23:58:51 +0500

    Added delete button successfully

--- routes.py
+++ routes.py
@@ -138,13 +138,13 @@ def register_routes(app, login_manager):
         return render_template("transactions.html", transactions=transactions)
     
 
-    @app.route("/delete/<init:id>", methods=["POST"])
+    @app.route("/delete/<int:id>", methods=["POST"])
     @login_required
     def delete(id):
         transactions = Transaction.query.get_or_404(id)
 
         if transactions.user_id != current_user.id:
-            flash("Unauthorized action", "denger")
+            flash("Unauthorized action", "danger")
             return redirect(url_for("dashboard"))
         
         db.session.delete(transactions)
